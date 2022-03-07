aa93757769d178c80661f82b8952a88fac1a0523
Thaycondrius <thiagoescarassatti@gmail.com>
2022-03-07 09:57:49 -0300

    Primeiro commit da Rest API com Spring

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    public List<Topico> lista() {
+
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+
+        return Arrays.asList(topico, topico, topico);
+
+    }
+
+}
