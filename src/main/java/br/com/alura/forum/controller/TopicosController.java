024cc41a08d89a4847697917631512839c5650b7
Thaycondrius <thiagoescarassatti@gmail.com>
2022-03-10 16:24:12 -0300

    Paginação melhorada com @PageableDefault

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -34,9 +36,7 @@ public class TopicosController {
 
     @GetMapping
     public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
-                                  @RequestParam int pagina, @RequestParam int qtd) {
-
-        Pageable paginacao = PageRequest.of(pagina,qtd);
+                                  @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
         if (nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
