52ad71c15230f10ac8c35ca7d4b082f63ff79af6
Thaycondrius <thiagoescarassatti@gmail.com>
2022-03-11 12:37:17 -0300

    Spring Security adicionando utilizando autenticação.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Awi2WI0WyjQ6hll8FAcBFek2yLTJ7eEgyJYba.RePc37LBwNhxIxy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
